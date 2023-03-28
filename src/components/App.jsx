c1afe6de725c117ac6ede18a5f5cb2de1caeb3b7
OlhaHobchuk <111050433+OlhaHobchuk@users.noreply.github.com>
2023-03-28 11:55:54 +0300

    Записує дані в localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,102 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import shortid from 'shortid';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Notification } from './Notification/Notification';
+
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  addContact = ({ name, number }) => {
+    const contact = {
+      id: shortid.generate(),
+      name,
+      number,
+    };
+    const isOnContacts = this.state.contacts.find(item => {
+      return item.name === contact.name;
+    });
+
+    if (isOnContacts) {
+      return alert(`${name} is already in contacts`);
+    }
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
+  filterChange = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  deleteContact = contactId => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
+    }));
+  };
+
+  getFilteredContacts = () => {
+    return this.state.contacts.filter(contact => {
+      return contact.name
+        .toLowerCase()
+        .includes(this.state.filter.toLowerCase());
+    });
+  };
+  render() {
+    const { contacts } = this.state;
+    const filteredContact = this.getFilteredContacts();
+
+    return (
+      <div
+        style={{
+          padding: '20px',
+          width: '500px',
+          borderStyle: 'solid',
+          borderColor: 'grey',
+          borderWidth: '2px',
+          borderRadius: '10px',
+        }}
+      >
+        <h1 style={{ marginTop: 0 }}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+
+        {contacts.length ? (
+          <>
+            <Filter onFilterChange={this.filterChange} />
+            <ContactList
+              contacts={filteredContact}
+              onDeleteContact={this.deleteContact}
+            />
+          </>
+        ) : (
+          <Notification message="There is no contacts" />
+        )}
+      </div>
+    );
+  }
+}
